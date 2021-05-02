af0942f2ec9d1528be08f233a2421d7df5cc8371
Nestor <nestor.venegas95@gmail.com>
2021-05-01 20:40:17 -0500

    commit tree 7f4280b3eecb38efba82e727ec8557cabe850d70

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,17 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate(){
+    this.clockTick()
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
 
   componentDidMount() {
     this.interval = setInterval(
